6d62bd6aa62643b88cfacf532504b46660fdaf1a
WilmarRamirez <wilmarraga@gmail.com>
2024-04-28 14:17:09 -0500

    se hace on click al info de servicio

---
+++ main.js
@@ -0,0 +1,9 @@
+
+class StickyNavigation {
+
+    onTabClick(event, element) {
+        event.preventDefault();
+        let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
+        $('html, body').animate({ scrollTop: scrollTop }, 2000);
+    }
+}
